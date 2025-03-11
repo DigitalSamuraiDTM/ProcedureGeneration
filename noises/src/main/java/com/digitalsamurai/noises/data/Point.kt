e5de540c5c10b59f3251b9928c4190963ad802e5
Andrew <nugaev.andrei@gmail.com>
2025-03-12 01:47:45 +0300

    Fix understanding of linear interpolation

---
+++ noises/src/main/java/com/digitalsamurai/noises/data/Point.kt
@@ -0,0 +1,6 @@
+package com.digitalsamurai.noises.data
+
+public data class Point (
+    public val x: Float,
+    public val y: Float,
+)
--- noises/src/test/java/com/digitalsamurai/monochrome/LinearInterpolatorTest.kt
+++ noises/src/test/java/com/digitalsamurai/monochrome/LinearInterpolatorTest.kt
@@ -2,6 +2,7 @@ package com.digitalsamurai.monochrome
 
 import android.animation.TimeInterpolator
 import android.view.animation.BaseInterpolator
+import com.digitalsamurai.noises.data.Point
 import com.digitalsamurai.noises.interpolators.linearInterpolation
 import org.junit.Test
 
@@ -15,41 +16,13 @@ class LinearInterpolatorTest {
     fun testLinearInterpolator() {
         // прямая ox = oy
         val x = 10f
-        val fX = {x: Float -> x }
-        val y = linearInterpolation(x, fX)
+        val y = linearInterpolation(
+            x = x,
+            point1 = Point(0.0f, 0.0f),
+            point2 = Point(11f, 11f)
+        )
         println(y)
         assert(y == 10f)
     }
 
-    @Test
-    fun testAlphaAnimation() {
-        // представим, что интерполируем alpha до 1.5f вьюшки за 400мс
-        val animationTime = 400L
-        val finishedAlpha = 1.5f
-        val interpolationAlpha = { currentAnimationTime: Float -> (currentAnimationTime / animationTime) * finishedAlpha }
-        // теперь можем брать каждую миллисекунду и получать по ней значение
-        var currentAlpha = 0f
-        val currentTime = System.currentTimeMillis()
-        val destinationTime = currentTime + animationTime
-        while (destinationTime >= System.currentTimeMillis()) {
-            val animationTime = animationTime - (destinationTime - System.currentTimeMillis())
-            currentAlpha = linearInterpolation(animationTime.toFloat(), interpolationAlpha)
-            Thread.sleep(1)
-            // можно заметить, что стоит знак >=
-            println(currentAlpha)
-        }
-
-        val linearAnimationInterpolator = object: BaseInterpolator() {
-            /**
-             * [p0] значение между 0 и 1.0, где 0.0 это начало анимации, а 1.0 это конец анимации (оно потом умножится на длительность анимации)
-             * @return интерполируемое значение
-             */
-            override fun getInterpolation(p0: Float): Float {
-                return p0
-            }
-
-        }
-
-        assert(true)
-    }
 }
