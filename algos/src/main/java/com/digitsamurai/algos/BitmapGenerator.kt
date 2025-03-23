57cbb553cef633f1ad61ef3af55cb3228f627826
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-03-24 02:08:00 +0300

    implement BitmapGenerator.neighborBitmap function

--- algos/src/main/java/com/digitsamurai/algos/BitmapGenerator.kt
+++ algos/src/main/java/com/digitsamurai/algos/BitmapGenerator.kt
@@ -9,11 +9,18 @@ import com.digitalsamurai.math.interpolators.Interpolation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.random.Random
+
+// TODO: дело в том, что картинка считается по координатам с левого верхнего угла вниз, а интерполяция в декартовой системе считается с левого нижнего угла вниз
+//  из-за этого картинка получаются перевернутыми в действительности. Надо это исправить
+
 
 class BitmapGenerator @Inject constructor() {
 
+    private fun newBitmapTemplate(size: Size): Bitmap = Bitmap.createBitmap(size.width, size.height, Bitmap.Config.ARGB_8888)
+
     suspend fun bilinearBitmap(size: Size, bilinearConfig: BilinearConfig): Bitmap = withContext(Dispatchers.Default) {
-        val bitmap = Bitmap.createBitmap(size.width, size.height, Bitmap.Config.ARGB_8888)
+        val bitmap = newBitmapTemplate(size)
         val colorLB = bilinearConfig.colorLB
         val colorRB = bilinearConfig.colorRB
         val colorLT = bilinearConfig.colorLT
@@ -55,7 +62,45 @@ class BitmapGenerator @Inject constructor() {
     }
 
     suspend fun neighborBitmap(size: Size, neighborConfig: NeighborConfig): Bitmap = withContext(Dispatchers.Default) {
-        TODO()
+        val entryPoints = when (neighborConfig) {
+            is NeighborConfig.Random -> {
+                val points = mutableMapOf<D3Point, Color>()
+                repeat(neighborConfig.countPoints) { i ->
+                    points[D3Point(
+                        x = Random.nextInt(size.width).toFloat(), // TODO локации точек могут повторяться, это плохо
+                        y = Random.nextInt(size.height).toFloat(),
+                        z = i.toFloat()
+                    )] = Color.valueOf(
+                        Random.nextInt(100) / 100f,
+                        Random.nextInt(100) / 100f,
+                        Random.nextInt(100) / 100f,
+                    )
+
+                }
+                points
+            }
+        }
+
+        val bitmap = newBitmapTemplate(size)
+
+        repeat(bitmap.width) { w ->
+            repeat(bitmap.height) { h ->
+                val nearestZ = Interpolation.TwoDimensional.nearestNeighbor(
+                    entryPoint = D2Point(w.toFloat(),h.toFloat()),
+                    points = entryPoints.keys
+                )
+                // используем z в качестве маркера, с помощью которого можно найти искомую точку, а по ее ключу достать цвет пикселя
+                // это нужно, чтобы не интерполироваться отдельно по a,r,g,b
+                val foundedColor = entryPoints[entryPoints.keys.first { it.z == nearestZ }] ?: throw IllegalStateException("Color was not founded")
+                bitmap.set(
+                    x = w,
+                    y = h,
+                    color = foundedColor.toArgb()
+                )
+            }
+        }
+
+        return@withContext bitmap
     }
 
     data class Size(
