35cd528ca64702b3d76db4b8724f0de34ec849ca
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-04-02 02:12:58 +0300

    try to cubic and bicubic interpolation

---
+++ math/src/main/java/com/digitalsamurai/math/interpolators/dimensionals/two/BicubicInterpolation.kt
@@ -0,0 +1,13 @@
+package com.digitalsamurai.math.interpolators.dimensionals.two
+
+import com.digitalsamurai.math.data.D2Point
+import com.digitalsamurai.math.data.D3Point
+import kotlin.math.ceil
+
+/**
+ * Бикубическая интерполяция
+ */
+fun bicubicInterpolation(entryPoint: D2Point, points: List<D3Point>): Float {
+
+
+}
