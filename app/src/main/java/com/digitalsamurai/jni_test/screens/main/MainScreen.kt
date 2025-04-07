68b009f0e71d461e0a7a5ff91e6008b4d3002aea
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-04-08 01:37:20 +0300

    fix paddings in grid cell

--- app/src/main/java/com/digitalsamurai/jni_test/screens/main/MainScreen.kt
+++ app/src/main/java/com/digitalsamurai/jni_test/screens/main/MainScreen.kt
@@ -1,10 +1,10 @@
 package com.digitalsamurai.jni_test.screens.main
 
-import android.content.Context
-import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -13,7 +13,6 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.digitalsamurai.jni_test.core.screen.BaseScreen
@@ -43,16 +42,16 @@ object MainScreen : BaseScreen<MainScreenState, MainScreenEvent, MainScreenActio
         state: MainScreenState,
         actions: MainScreenActions,
     ) {
-        Log.d("OBAMA", "${LocalContext.current.getDir("bitmaps", Context.MODE_PRIVATE).list().joinToString(", ")}")
         Box(modifier = Modifier.fillMaxSize()) {
             LazyVerticalGrid(
                 modifier = Modifier.fillMaxSize(),
                 columns = GridCells.Fixed(2),
-                contentPadding = PaddingValues(8.dp)
+                contentPadding = PaddingValues(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(state.featuresItems) { feature ->
                     FeatureItem(
-                        modifier = Modifier.height(110.dp),
+                        modifier = Modifier.height(110.dp).fillMaxWidth(),
                         state = feature,
                         onClick = { actions.onFeatureItemClicked(feature.id) }
                     )
